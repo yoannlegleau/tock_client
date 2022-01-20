5ce5604b14a193d4b018456bb68a0627c874e18b
yoann LE GLEAU <81438255+yoannlegleau@users.noreply.github.com>
2022-01-20 15:40:14 +0100

    update doc

---
+++ doc/html/search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['wincreate_0',['winCreate',['../main_s_d_l_8h.html#ad0d09c1b6009b869b52530dcf379eb51',1,'mainSDL.c']]],
+  ['windestroy_1',['winDestroy',['../main_s_d_l_8c.html#aaf044c965a62de4320efd57d1ad0fa81',1,'winDestroy():&#160;mainSDL.c'],['../main_s_d_l_8h.html#aaf044c965a62de4320efd57d1ad0fa81',1,'winDestroy():&#160;mainSDL.c']]],
+  ['window_2',['window',['../main_s_d_l_8c.html#a04a8a2bbfa9c15500892b8e5033d625b',1,'mainSDL.c']]]
+];
---
+++ doc/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['window_0',['window',['../main_s_d_l_8c.html#a04a8a2bbfa9c15500892b8e5033d625b',1,'mainSDL.c']]]
+];
